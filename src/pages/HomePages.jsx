32673d124b17e3fd8d09e36a677c856b84bbba37
Tomas Jerez <tomasjerezjavier3@gmail.com>
2025-04-26 15:16:12 -0300

    FINAL

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,21 +1,24 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import '../Styles/HomePages.css'; // Importa tu archivo CSS aquí
-
 
 export default function HomePages() {
   return (
-    <div className="page-container fade-in">
-      <div className="content-wrap">
+    <div className="homepage-background">
+      <div className="page-container fade-in">
         <Header />
-        <div className="conteiner2">
-        <h1 id="home-title" className="fade-in fade-in-delay-1">BIENVENIDO AL INICIO</h1>
-        <p className="fade-in fade-in-delay-2">Este es el inicio de la pagina</p>
-        <p className="fade-in fade-in-delay-3">Para ver los integrantes del grupo, dirigete a la pestaña de nosotros</p>
-        <p className="fade-in fade-in-delay-3">Para agregar un nuevo integrante, dirigete a la pestaña de agregar miembro</p>
+        <div className="content-wrap">
+          <div className="conteiner2">
+            <h1 id="home-title" className="fade-in fade-in-delay-1">BIENVENIDO AL INICIO</h1>
+            <p className="fade-in fade-in-delay-2">Este es el inicio de la página</p>
+            <p className="fade-in fade-in-delay-3">Para ver los integrantes del grupo, dirígete a la pestaña de nosotros</p>
+            <p className="fade-in fade-in-delay-3">Para agregar un nuevo integrante, dirígete a la pestaña de agregar miembro</p>
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   )
 }
+
+
